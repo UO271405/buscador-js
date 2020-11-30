508c32da2f84ea9c1601909334c18846e554fa69
UO271405 <60234300+UO271405@users.noreply.github.com>
2020-11-30 23:09:36 +0100

    Add files via upload

--- Ejercicio10.js
+++ Ejercicio10.js
@@ -38,42 +38,6 @@ class NewsSearcher{
             })
         })
     }
-
-    cargarDatos(ciudad, codigoPais){
-        var url = "https://api.openweathermap.org/data/2.5/weather?q=" + ciudad + this.tipo + this.unidades + this.idioma + "&APPID=" + this.apikey;
-        
-        $.ajax({
-            dataType: "json",
-            url: url,
-            method: 'GET',
-            success: function(datos){
-                //$('.json').text(JSON.stringify(datos, null, 2));
-
-                $('.datos').empty();    
-                $('.datos').append('<img src="https://openweathermap.org/img/w/' + datos.weather[0].icon + '.png" height="64px" width="64px">');
-                $('.datos').append("<p>Ciudad: " + datos.name + "<br>");
-                $('.datos').append("País: " + datos.sys.country + "<br>");
-                $('.datos').append("Latitud: " + datos.coord.lat + " grados<br>");
-                $('.datos').append("Longitud: " + datos.coord.lon + " grados<br>");
-                $('.datos').append("Temperatura: " + datos.main.temp + " grados Celsius<br>");
-                $('.datos').append("Máx temperatura: " + datos.main.temp_max + " celsius<br>");
-                $('.datos').append("Min temperatura: " + datos.main.temp_min + " celsius<br>");
-                $('.datos').append("Presión: " + datos.main.pressure + " milímetros<br>");
-                $('.datos').append("Humedad: " + datos.main.humidity + "%<br>"); 
-                $('.datos').append("Amanece a las: " + new Date(datos.sys.sunrise *1000).toLocaleTimeString() + "<br>"); 
-                $('.datos').append("Oscurece a las: " + new Date(datos.sys.sunset *1000).toLocaleTimeString() + "<br>"); 
-                $('.datos').append("Velocidad del viento: " + datos.wind.speed + "metros/segundo<br>"); 
-                $('.datos').append("Dirección del viento: " + datos.wind.deg +"grados<br>");
-                $('.datos').append("Fecha de la medida: " + new Date(datos.dt *1000).toLocaleDateString() + "<br>");
-                $('.datos').append("Descripción: " + datos.weather[0].description + "<br>");
-                $('.datos').append("Visibilidad: " + datos.visibility + " metros<br>");
-                $('.datos').append("Nubosidad: " + datos.clouds.all + " %</p>");
-            },
-            error:function(){
-                document.write(this.error);    
-            }
-        });
-    }
 }
 
 var searcher = new NewsSearcher();
